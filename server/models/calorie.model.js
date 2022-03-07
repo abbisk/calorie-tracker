996e25b20544818dd10a0aa861077b7ab53af4e6
abbisk <abbiskpro@gmail.com>
2022-03-07 11:40:25 +0530

    logic added for calories fetch update and delete

---
+++ server/models/calorie.model.js
@@ -0,0 +1,32 @@
+//models/calorie.model.js
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const calorieSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    calories: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Calorie = mongoose.model("CalorieJournal", calorieSchema);
+
+module.exports = Calorie;
